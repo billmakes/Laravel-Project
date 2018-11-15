d879e272951f4a60826c17803c563349ab7e72d3
Bill <hilbertwilliam@gmail.com>
2018-11-14 22:36:56 -0500

    init

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Projects</title>
+    <style>
+        .is-complete {
+            text-decoration: line-through;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+</head>
+<body>
+    <section class="section">
+        <div class="container">
+            @yield('content')
+        </div>
+    </section>
+</body>
+</html>
